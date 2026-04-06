e6e6ae3ae74b99d34c4a965671b0422ff9f65214
omirek <140943274+omirek@users.noreply.github.com>
2026-04-06 12:21:24 +0200

    Add user ID to Session interface in auth.ts
    Extend the Session interface to include user ID.

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,15 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "@/lib/db";
 
+// To mówi TypeScriptowi, że w obiekcie session.user znajdzie się pole 'id'
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+    } & DefaultSession["user"]
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as any,
   providers: [
